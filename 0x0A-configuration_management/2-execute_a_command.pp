e516568130dc433d43e3acc87cae039f45454f0f
yvonnegichovi <yvonnegichovi@gmail.com>
2024-02-26 23:58:37 -0800

    2

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,8 +1,6 @@
 #Create a manifest that kills a proceed named killmenow
 
-exec { 'killmenow':
-  command     => '/usr/bin/pkill -f killmenow',
-  path        => ['/usr/bin'],
-  onlyif      => '/usr/bin/pgrep -f killmenow',
-  refreshonly => true,
+exec { 'pkill':
+  command  => 'pkill  killmenow',
+  provider => 'shell',
 }
