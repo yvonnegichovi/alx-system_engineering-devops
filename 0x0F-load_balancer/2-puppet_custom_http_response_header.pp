811c72314f917385c8410f3b75239e6565ba68d3
yvonnegichovi <yvonnegichovi@gmail.com>
2024-03-17 03:13:19 -0700

    1

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -6,25 +6,40 @@ package { 'nginx':
 # Define custom HTTP header value (server hostname)
 $hostname_value = $::hostname
 
-# Configure Nginx to add the custom header
-file { '/etc/nginx/sites-available/default':
+# Replace Nginx default configuration
+file { '/etc/nginx/sites-enabled/default':
    ensure => file,
-   content => template('nginx/default.erb'),
+   source => 'puppet:///modules/nginx/nginx.conf',
    require => Package['nginx'],
    notify => Service['nginx'],
 }
 
-# Enable default site
-file { '/etc/nginx/sites-enabled/default':
-   ensure => link,
-   target => '/etc/nginx/sites-available/default',
+# Configure Nginx to add the custom header
+file { '/etc/nginx/sites-available/default':
+   ensure => file,
+   content => 'server {
+		listen 80 default_server;
+		listen [::]:80 default_server;
+		server_name _;
+		location / {
+		   add_header X-Servedn-By $hostname;
+		   root /var/www/html;
+		   index index.html;
+		}
+	      }',
    require => Package['nginx'],
    notify => Service['nginx'],
+}
+
+file { '/etc/nginx/nginx.conf':
+   ensure  => file,
+   source  => 'puppet:///modules/nginx/nginx.conf',
 }
 
 # Define nginx service
 service { 'nginx':
    ensure => running,
    enable => true,
-   subscribe => [File['/etc/nginx/sites-available/default'], File['/etc/nginx/nginx.conf']]
+   require => Package['nginx'],
+   subscribe => [File['/etc/nginx/sites-available/default', '/etc/nginx/nginx.conf']]
 }
