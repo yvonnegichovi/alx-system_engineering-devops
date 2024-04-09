507f62833b69a0f674a631fca45759c62823b265
yvonnegichovi <yvonnegichovi@gmail.com>
2024-04-09 22:53:45 +0300

    task 0.4

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,7 +8,7 @@ import json
 def number_of_subscribers(subreddit):
     """Returns the total number of subscribers"""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "Mozilla/5.0"}
+    headers = {"User-Agent": "Python/requests"}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
