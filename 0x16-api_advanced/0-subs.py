ba3a306448efc2ebd9ebfeac4f66d75039e43fa9
yvonnegichovi <yvonnegichovi@gmail.com>
2024-04-09 22:36:24 +0300

    task 0.1

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,11 +2,12 @@
 """This module queries Reddit API and returns the number of subscribers"""
 
 import requests
+import json
 
 
 def number_of_subscribers(subreddit):
     """Returns the total number of subscribers"""
-    url = f"https://www.reddit.com/dev/api/"
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
