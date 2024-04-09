ac32ac61bb235998fd7e258d018070a0b71ca0a2
yvonnegichovi <yvonnegichovi@gmail.com>
2024-04-09 22:25:04 +0300

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,2 +1,16 @@
 #!/usr/bin/python3
 """This module queries Reddit API and returns the number of subscribers"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Returns the total number of subscribers"""
+    url = f"https://www.reddit.com/dev/api/"
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        return data['data']['subscribers']
+    else:
+        return 0
