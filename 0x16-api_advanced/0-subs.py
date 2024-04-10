3d1b2683dcc7b9d98d47e3a19251081a78047ee5
yvonnegichovi <yvonnegichovi@gmail.com>
2024-04-10 13:43:29 +0300

    task 0.5

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """This module queries Reddit API and returns the number of subscribers"""
 
-import requests
 import json
+import requests
 
 
 def number_of_subscribers(subreddit):
