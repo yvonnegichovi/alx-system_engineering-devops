0f7d6ccb55db90a55187afbac2f57ac9085990f4
yvonnegichovi <yvonnegichovi@gmail.com>
2024-04-14 12:20:23 +0000

    fixing php extansion

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,14 +1,5 @@
 # Using strace, find out why Apache is returning a 500 error.
-file { '/tmp/apache_strace.txt':
-  ensure => present,
-}
-exec { 'strace_apache':
-  command     => '/usr/bin/strace -o /tmp/apache_strace.txt -f -p $(pidof apache2)',
-  refreshonly => true,
-}
-
-exec { 'restart_apache_on_strace':
-  command     => '/bin/systemctl restart apache2',
-  subscribe   => File['/tmp/apache_strace.txt'],
-  refreshonly => true,
+exec { 'fix php extension':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin',
 }
